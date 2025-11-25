7aef86d1eeaadc316dfb89f6392797c03a3a2ca5
amatunana <96621457+amatunana@users.noreply.github.com>
2025-11-25 20:23:12 +0900

    re

--- Assets/_Scripts/player.cs
+++ Assets/_Scripts/player.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public int MAXHP = 200; //最大HP
-    public int MAXMP = 50; //最大MP
-    public int ATK = 10; //攻撃力
-    public int DEF = 5; //防御力
+    public static int MAXHP = 200 ; //最大HP
+    public static int ATK = 10; //攻撃力
+    public static int DEF = 5; //防御力
+    public static int SKILL = 0; //スキルポイント
 }
--- Assets/_Scripts/test.cs
+++ Assets/_Scripts/test.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    
+    public int MAXHP = player.MAXHP;
+    public int ATK = player.ATK;
+    public int DEF = player.DEF;
+    public int SKILL = player.SKILL;
+    Dictionary<string, int> item_dict = new Dictionary<string, int>(){
+        {"ガトリング", 0},
+        {"大砲", 0},
+        {"スタンガン", 0}      
+    };
+
+    public test TEST;
+    public battle_system Battel_System;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,5 +31,8 @@ public class test : MonoBehaviour
         
     }
 
-
+    public void attack()
+    {
+        
+    }
 }

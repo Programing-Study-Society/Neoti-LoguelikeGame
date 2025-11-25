e6522785ce461e84d23e801a02a6651882fbfafc
amatunana <96621457+amatunana@users.noreply.github.com>
2025-11-25 18:44:06 +0900

    re

---
+++ Assets/_Scripts/player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public int MAXHP = 200; //最大HP
+    public int MAXMP = 50; //最大MP
+    public int ATK = 10; //攻撃力
+    public int DEF = 5; //防御力
+}

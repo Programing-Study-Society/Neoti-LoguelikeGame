f55d38be81c47943b2e3a34f36387d44e48447e9
amatunana <96621457+amatunana@users.noreply.github.com>
2025-12-02 19:49:32 +0900

    コメント文

---
+++ Assets/_Scripts/skill_L.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skill_L : MonoBehaviour
+{
+    public Dictionary<string, List<List<int>>> skill_list = new Dictionary<string, List<List<int>>>(){
+    {"attaku", new List<List<int>>(){
+        new List<int>(){1,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0}
+        }
+    },
+    {"defense", new List<List<int>>(){
+        new List<int>(){1,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0}
+        }
+    },
+    {"hp", new List<List<int>>(){
+        new List<int>(){1,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0},
+        new List<int>(){0,0}
+        }
+    }
+    };
+}

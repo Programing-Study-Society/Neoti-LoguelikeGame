6735901433f16b6e35a92b26e543d0c28ab0aef2
amatunana <96621457+amatunana@users.noreply.github.com>
2025-12-02 19:13:35 +0900

    コメント文

--- Assets/_Scripts/test.cs
+++ Assets/_Scripts/test.cs
@@ -4,19 +4,8 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    
-    public int MAXHP = player.MAXHP;
-    public int ATK = player.ATK;
-    public int DEF = player.DEF;
-    public int SKILL = player.SKILL;
-    Dictionary<string, int> item_dict = new Dictionary<string, int>(){
-        {"ガトリング", 0},
-        {"大砲", 0},
-        {"スタンガン", 0}      
-    };
-
     public test TEST;
-    public battle_system Battel_System;
+    //public battle_system Battel_System;
    
 
     // Start is called before the first frame update

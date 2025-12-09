60ec87bed8a8f8d9813d4906174c825435bafa0a
amatunana <96621457+amatunana@users.noreply.github.com>
2025-12-09 16:52:53 +0900

    コメント文

--- Assets/_Scripts/skill_L.cs
+++ Assets/_Scripts/skill_L.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class skill_L : MonoBehaviour
 {
+    //{アクティブ状態 , 取得状態}
     public Dictionary<string, List<List<int>>> skill_list = new Dictionary<string, List<List<int>>>(){
     {"attaku", new List<List<int>>(){
         new List<int>(){1,0},

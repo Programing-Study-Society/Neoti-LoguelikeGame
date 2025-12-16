77244d54ec6fc5de4f6b9d7ec89ceb41be8b765d
amatunana <96621457+amatunana@users.noreply.github.com>
2025-12-16 18:42:21 +0900

    コメント文

--- Assets/_Scripts/summary.cs
+++ Assets/_Scripts/summary.cs
@@ -6,23 +6,78 @@ using UnityEngine;
 
 public class summary : MonoBehaviour
 {
-    public void title()
+    public enum GameState
     {
-        
+        Title,
+        Map,        // マップ上で移動
+        Battle,     // バトル中
+        Result,     // バトル後の報酬
+        Treasure,   // 宝箱
+        Shop        // ショップ
+    }
+
+    public GameState currentState;
+
+    void Start()
+    {
+        ChangeState(GameState.Map);
+    }
+
+
+        public void ChangeState(GameState newState)
+    {
+        currentState = newState;
+
+        switch (newState)
+        {
+            case GameState.Title:
+                Title();
+                break;
+            case GameState.Map:
+                Map();
+                break;
+
+            case GameState.Battle:
+                Battle();
+                break;
+
+            case GameState.Result:
+                Result();
+                break;
+
+            case GameState.Treasure:
+                Box();
+                break;
+
+            case GameState.Shop:
+                Shop();
+                break;
+        }
     }
     
-    public void battle()
+
+    public void Title()
     {
         
     }
-
-    public void map()
+    public void Map()
     {
         
     }
-
+    public void Battle()
+    {
+        
+    }
+    public void Result()
+    {
+        
+    }
     public List<int> Box()
     {
         return new List<int>() {0,0,0};
+    }
+    public void Shop()
+    {
+        
     }
 }

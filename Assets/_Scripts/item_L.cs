586737dc5baca87759fa9b272b714b3af1927c7c
amatunana <96621457+amatunana@users.noreply.github.com>
2025-12-16 19:04:49 +0900

    コメント文

--- Assets/_Scripts/item_L.cs
+++ Assets/_Scripts/item_L.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class item : MonoBehaviour
 {
-    public Dictionary<int,str, List<int>> item_list = new Dictionary<int, List<int>>(){
-    {0,"", new List<int>() {0,0,0}},
-    {1,"", new List<int>() {0,0,0}},
-    {2,"", new List<int>() {0,0,0}},
-    {3,"", new List<int>() {0,0,0}},
-    {4,"", new List<int>() {0,0,0}},
-    {5,"", new List<int>() {0,0,0}},
-    {6,"", new List<int>() {0,0,0}},
-    {7,"", new List<int>() {0,0,0}},
-    {8,"", new List<int>() {0,0,0}}
+    public Dictionary<int, List<int>> item_list = new Dictionary<int, List<int>>(){
+    {0, new List<int>() {0,0,0}},
+    {1, new List<int>() {0,0,0}},
+    {2, new List<int>() {0,0,0}},
+    {3, new List<int>() {0,0,0}},
+    {4, new List<int>() {0,0,0}},
+    {5, new List<int>() {0,0,0}},
+    {6, new List<int>() {0,0,0}},
+    {7, new List<int>() {0,0,0}},
+    {8, new List<int>() {0,0,0}}
     };
 }

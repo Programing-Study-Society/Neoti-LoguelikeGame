cf0e175e17cc08e8976a4e2feb731e75945f6ab6
amatunana <96621457+amatunana@users.noreply.github.com>
2025-12-22 19:10:29 +0900

    スキルtest

---
+++ Assets/_Scripts/skill.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skill : MonoBehaviour
+{
+    public void skill_push(int typ)
+    {
+        
+    }
+
+    public void skill_in(int typ)
+    {
+        switch (typ)
+        {
+            case 1:
+                
+                break;
+        }
+    }
+}

b46fee936d56014ec4fddbd8d7e182b389da3365
amatunana <96621457+amatunana@users.noreply.github.com>
2025-12-23 20:47:59 +0900

    a

--- Assets/_Scripts/enemy_L.cs
+++ Assets/_Scripts/enemy_L.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class enemy_L : MonoBehaviour
 {
     public int MAXHP = 0 ; //最大HP
-    public int HP = 
+    public int HP = 0;//リアルタイムHP
     public int ATK = 0; //攻撃力
     public int DEF = 0; //防御力
 
--- Assets/_Scripts/player.cs
+++ Assets/_Scripts/player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public int MAXHP = 200 ; //最大HP
+    public int HP;
     public int ATK = 10; //攻撃力
     public int DEF = 5; //防御力
     public int SKILL = 0; //スキルポイント

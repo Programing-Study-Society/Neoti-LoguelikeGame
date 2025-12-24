dd449b26e384c9956fedc64d831b169931d3ce47
amatunana <96621457+amatunana@users.noreply.github.com>
2025-12-24 17:57:30 +0900

    a

--- Assets/_Scripts/enemy_L.cs
+++ Assets/_Scripts/enemy_L.cs
@@ -8,5 +8,12 @@ public class enemy_L : MonoBehaviour
     public int HP = 0;//リアルタイムHP
     public int ATK = 0; //攻撃力
     public int DEF = 0; //防御力
+    public List<int> BUFF = new List<int>() {
+    0,//持続ダメージ(デバフ)
+    0,//ダメージダウン(デバフ)
+    0,//ダメージアップ(バフ)
+    0,//防御力ダウン(デバフ)
+    0//防御力アップ(バフ)
+    };
 
 }
